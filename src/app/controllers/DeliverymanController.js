c52b6b28ee977aae27c4d2d9dc596cc62379c697
irailtonreis <irailton.oliveira91@gmail.com>
2020-03-14 12:30:29 -0300

    Adicionando métodos index, store, update e delete deliveryman

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -2,21 +2,31 @@ import Deliverman from '../models/Deliveryman';
 
 class DeliverymanController {
   async index(req, res) {
-    return res.json(req.body);
+    const deliverymen = await Deliverman.findAll();
+    return res.json(deliverymen);
   }
 
   async store(req, res) {
-    // const { avatar_id, name, email } = req.body;
-
     const deliveryman = await Deliverman.create(req.body);
     return res.json(deliveryman);
   }
 
   async update(req, res) {
-    return res.send();
+    const deliveryman = await Deliverman.findByPk(req.params.id);
+
+    const { id, name, email, avatar_id } = await deliveryman.update(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      avatar_id,
+    });
   }
 
   async delete(req, res) {
+    const deliverymen = await Deliverman.findByPk(req.params.id);
+
+    deliverymen.destroy();
     return res.send();
   }
 }
--- src/app/models/Deliveryman.js
+++ src/app/models/Deliveryman.js
@@ -1,6 +1,6 @@
 import Sequelize, { Model } from 'sequelize';
 
-class Deliverymans extends Model {
+class Deliveryman extends Model {
   static init(sequelize) {
     super.init(
       {
@@ -20,4 +20,4 @@ class Deliverymans extends Model {
   }
 }
 
-export default Deliverymans;
+export default Deliveryman;
