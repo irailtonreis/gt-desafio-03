e1359cf649567a624182e3db9cbd79fcc168c191
irailtonreis <irailton.oliveira91@gmail.com>
2020-03-15 16:38:14 -0300

    Validações deliveryman método store e update

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -1,4 +1,6 @@
+import * as Yup from 'yup';
 import Deliverman from '../models/Deliveryman';
+import File from '../models/File';
 
 class DeliverymanController {
   async index(req, res) {
@@ -7,14 +9,60 @@ class DeliverymanController {
   }
 
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      avatar_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
+    const deliverymanExists = await Deliverman.findOne({
+      where: { email },
+    });
+
+    if (deliverymanExists) {
+      return res.status(400).json({ error: 'E-mail already exists.' });
+    }
+
     const deliveryman = await Deliverman.create(req.body);
+
     return res.json(deliveryman);
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      avatar_id: Yup.number().required(),
+    });
+
     const deliveryman = await Deliverman.findByPk(req.params.id);
 
-    const { id, name, email, avatar_id } = await deliveryman.update(req.body);
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
+
+    if (req.body.email !== deliveryman.email) {
+      const deliverymanExists = await Deliverman.findOne({
+        where: { email },
+      });
+
+      if (deliverymanExists) {
+        return res.status(400).json({ error: 'E-mail already exists.' });
+      }
+    }
+
+    const { id, name, avatar_id } = await deliveryman.update(req.body);
     return res.json({
       id,
       name,
@@ -24,9 +72,11 @@ class DeliverymanController {
   }
 
   async delete(req, res) {
-    const deliverymen = await Deliverman.findByPk(req.params.id);
+    const deliverymen = await Deliverman.findByPk(req.params.id, {
+      include: [{ model: File, as: 'avatar' }],
+    });
 
-    deliverymen.destroy();
+    await deliverymen.destroy();
     return res.send();
   }
 }
