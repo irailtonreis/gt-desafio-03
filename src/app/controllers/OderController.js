6ab20a8d9dbdcd93244342bc644e1a1311f20e5c
irailtonreis <irailton.oliveira91@gmail.com>
2020-03-18 17:18:30 -0300

    Adicionando validação data

--- src/app/controllers/OderController.js
+++ src/app/controllers/OderController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Order from '../models/Order';
 
 class OrderController {
@@ -18,6 +19,29 @@ class OrderController {
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
+    }
+    const { start_date, end_date, deliveryman_id } = req.body;
+
+    const hourStart = startOfHour(parseISO(start_date));
+    console.log(hourStart);
+    // const hourEnd = endOfHour(parseISO(end_date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const checkOrders = await Order.findOne({
+      where: {
+        deliveryman_id,
+        canceled_at: false,
+        hourStart,
+      },
+    });
+
+    if (checkOrders) {
+      return res
+        .status(400)
+        .json({ error: 'There is already a delivery in progress' });
     }
 
     const order = await Order.create(req.body);
