39ac293a3613336908e59d46f99503702fdc889b
irailtonreis <irailton.oliveira91@gmail.com>
2020-03-23 17:21:13 -0300

    finalizando store orders

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,17 +1,5 @@
 import * as Yup from 'yup';
-import {
-  parseISO,
-  startOfHour,
-  isBefore,
-  setSeconds,
-  setMinutes,
-  setHours,
-  format,
-  isAfter,
-  isBefore,
-  startOfDay,
-  endOfDay,
-} from 'date-fns';
+import { parseISO, startOfHour, isBefore, format, isAfter } from 'date-fns';
 import Order from '../models/Order';
 
 class OrderController {
@@ -28,69 +16,48 @@ class OrderController {
       deliveryman_id: Yup.number().required(),
     });
 
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { deliveryman_id, start_date } = req.body;
 
-    const startDate = [
-      '08:00',
-      '18:00',
-    ];
-    const [date, minute] = start_date.split('T');
-    console.log(date);
+    const date = parseISO(start_date);
 
-    var startday = startOfDay(new Date(date, 8, 00, 0))
-    var endDay = startOfDay(new Date(date, 18, 00, 0))
+    const date1 = parseISO(start_date).setHours(5, 0, 0);
 
-    console.log(startday);
-    // console.log(result);
-    // const avaliable = startDate.map((time, index) => {
-      
+    const date2 = parseISO(start_date).setHours(15, 0, 0);
 
-      const [hour] = time.split(':');
+    const startDay = parseISO(format(date1, "yyy-MM-dd'T'HH:mm:ssxxx"));
+    const endDay = parseISO(format(date2, "yyy-MM-dd'T'HH:mm:ssxxx"));
 
-    //   console.log(hour);
-    //   const hourEnd = setSeconds(
-    //     setMinutes(setHours(parseISO(start_date), hour), minute),
-    //     0
-    //   );
+    if (isBefore(date, startDay)) {
+      return res.status(400).json({ error: 'Time not allowed' });
+    }
 
+    if (isAfter(date, endDay)) {
+      return res.status(400).json({ error: 'Time not allowed' });
+    }
 
-    //   const date = parseISO(start_date);
+    const hourStart = startOfHour(parseISO(start_date));
 
-    //   return {
-    //     value: format(value, "yyy-MM-dd'T'HH:mm:ssxxx"),
-    //     date: format(date, "yyy-MM-dd'T'HH:mm:ssxxx"),
-    //     avaliable: isBefore(date, hourEnd),
-    //   };
-    // });
+    const checkOrders = await Order.findOne({
+      where: {
+        deliveryman_id,
+        canceled_at: null,
+        start_date: hourStart,
+      },
+    });
 
+    if (checkOrders) {
+      return res
+        .status(400)
+        .json({ error: 'There is already a delivery in progress' });
+    }
 
-    // if (!(await schema.isValid(req.body))) {
-    //   return res.status(400).json({ error: 'Validation fails' });
-    // }
+    const order = await Order.create(req.body);
 
-    // const hourStart = startOfHour(parseISO(start_date));
-
-    // if (isBefore(hourStart, new Date())) {
-    //   return res.status(400).json({ error: 'Past dates are not permitted' });
-    // }
-
-    // const checkOrders = await Order.findOne({
-    //   where: {
-    //     deliveryman_id,
-    //     canceled_at: null,
-    //     start_date: hourStart,
-    //   },
-    // });
-
-    // if (checkOrders) {
-    //   return res
-    //     .status(400)
-    //     .json({ error: 'There is already a delivery in progress' });
-    // }
-
-    // const order = await Order.create(req.body);
-
-    return res.json(startday);
+    return res.send(order);
   }
 }
 
