c6318d15223281a7ee7467dfe5352b76fbc7493d
irailtonreis <irailton.oliveira91@gmail.com>
2020-04-03 12:06:43 -0300

    refactor: update metodos controller orders

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,5 +1,4 @@
 import * as Yup from 'yup';
-import { parseISO, startOfHour, isBefore, format, isAfter } from 'date-fns';
 import Order from '../models/Order';
 import Deliveryman from '../models/Deliveryman';
 import Recipient from '../models/Recipient';
@@ -61,46 +60,9 @@ class OrderController {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const { deliveryman_id, start_date, recipient_id, product } = req.body;
+    const { deliveryman_id, recipient_id, product } = req.body;
 
     const deliveryman = await Deliveryman.findByPk(deliveryman_id);
-
-    if (start_date) {
-      const date = parseISO(start_date);
-
-      const setDate = parseISO(start_date);
-
-      const date1 = setDate.setHours(5, 0, 0);
-
-      const date2 = setDate.setHours(15, 0, 0);
-
-      const startDay = parseISO(format(date1, "yyy-MM-dd'T'HH:mm:ssxxx"));
-      const endDay = parseISO(format(date2, "yyy-MM-dd'T'HH:mm:ssxxx"));
-
-      if (isBefore(date, startDay)) {
-        return res.status(400).json({ error: 'Hour is not allowed' });
-      }
-
-      if (isAfter(date, endDay)) {
-        return res.status(400).json({ error: 'Hour is not allowed' });
-      }
-
-      const hourStart = startOfHour(parseISO(start_date));
-
-      const checkOrders = await Order.findOne({
-        where: {
-          deliveryman_id,
-          canceled_at: null,
-          start_date: hourStart,
-        },
-      });
-
-      if (checkOrders) {
-        return res
-          .status(400)
-          .json({ error: 'There is already a delivery in progress' });
-      }
-    }
 
     const recipient = await Recipient.findByPk(recipient_id);
 
@@ -132,9 +94,7 @@ class OrderController {
       recipient_id: Yup.number().required(),
       deliveryman_id: Yup.number().required(),
     });
-    const { start_date, end_date } = req.body;
 
-    const hourEnd = startOfHour(parseISO(end_date));
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
@@ -148,17 +108,23 @@ class OrderController {
 
   async delete(req, res) {
     const schema = Yup.object().shape({
-      id: Yup.string().required(),
+      id: Yup.number().required(),
     });
 
     if (!(await schema.isValid(req.params))) {
-      return res.status(400).json({ error: 'Invalid ID' });
+      return res.status(400).json({ error: 'Invalid Id' });
     }
+
     const order = await Order.findByPk(req.params.id);
 
-    await order.destroy();
+    if (order.start_date) {
+      return res.status(401).json({ error: 'Cancellation is not allowed' });
+    }
 
-    return res.send();
+    order.canceled_at = new Date();
+    await order.save();
+
+    return res.json(order);
   }
 }
 
